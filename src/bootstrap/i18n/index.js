3cbccabea03b1c2f02ed1d1e8b0abb39fa112a17
Mix <mixchatmix3@gmail.com>
2017-11-27 15:33:31 -0300

    HttpStatus

--- src/bootstrap/i18n/index.js
+++ src/bootstrap/i18n/index.js
@@ -55,6 +55,60 @@ export const crud = {
 }
 
 /**
+ * @type {Array}
+ * @default httpStatus
+ */
+export const httpErrors = [
+  {200: 'Ok'},
+  {201: 'Criado'},
+  {202: 'Aceito'},
+  {203: 'Não autorizado'},
+  {204: 'Nenhum conteúdo'},
+  {205: 'Reset'},
+  {206: 'Conteúdo parcial'},
+  {207: 'Status Multi'},
+  {300: 'Múltipla escolha'},
+  {301: 'Movido'},
+  {302: 'Movido Temporariamente'},
+  {304: 'Não modificado'},
+  {305: 'Use Proxy'},
+  {306: 'Proxy Switch'},
+  {307: 'Redirecionamento temporário'},
+  {400: 'Requisição inválida'},
+  {401: 'Não autorizado'},
+  {402: 'Pagamento necessário'},
+  {403: 'Proibido'},
+  {404: 'Não encontrado'},
+  {405: 'Método não permitido'},
+  {406: 'Não Aceitável'},
+  {407: 'Autenticação de proxy necessária'},
+  {408: 'Tempo de requisição esgotou (Timeout)'},
+  {409: 'Conflito'},
+  {410: 'Gone'},
+  {411: 'Comprimento necessário'},
+  {412: 'Pré-condição falhou'},
+  {413: 'Entidade de solicitação muito grande'},
+  {414: 'Pedido-URI Too Long'},
+  {415: 'Tipo de mídia não suportado'},
+  {416: 'Solicitada de Faixa Não Satisfatória'},
+  {417: 'Falha na expectativa'},
+  {418: 'Eu sou um bule de chá'},
+  {422: 'Entidade improcessável'},
+  {423: 'Fechado'},
+  {424: 'Falha de Dependência'},
+  {425: 'Coleção não ordenada'},
+  {426: 'Upgrade Obrigatório'},
+  {450: 'Bloqueados pelo Controle de Pais do Windows'},
+  {499: 'Cliente fechou Pedido'},
+  {500: 'Erro interno do servidor'},
+  {501: 'Não implementado'},
+  {502: 'Bad Gateway'},
+  {503: 'Serviço indisponível'},
+  {504: 'Gateway Time-Out'},
+  {505: 'HTTP Version not supported'}
+]
+
+/**
  * @type {Object}
  */
 export const locales = {
